8f2a993cff1a97878c7161391e2744dd1136e855
RTayyipC52 <tayyipcicek52@gmail.com>
2022-05-04 21:40:50 +0300

    Swagger implementasyonunu gerçekleştir

--- project/src/main/java/web/project/ProjectApplication.java
+++ project/src/main/java/web/project/ProjectApplication.java
@@ -2,7 +2,13 @@ package web.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class ProjectApplication {
 
@@ -10,4 +16,11 @@ public class ProjectApplication {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("web.project"))
+				.build();
+	}
 }
